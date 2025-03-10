ed163eeb020942d4006db6161967f5dd6bcbfa66
Андрей Кан <andrey.a.kan86@gmail.com>
2025-03-10 13:54:39 +0300

    Final 2

--- app/src/main/java/com/example/dhammapada/ui/composables/mainscreen/MainScreen.kt
+++ app/src/main/java/com/example/dhammapada/ui/composables/mainscreen/MainScreen.kt
@@ -20,7 +20,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
@@ -31,18 +33,20 @@ import com.example.dhammapada.ui.theme.TextStyle
 import com.example.dhammapada.ui.viewmodel.DhammapadaViewModel
 
 @Composable
-fun MainScreen(viewModel: DhammapadaViewModel = viewModel()){
+fun MainScreen(viewModel: DhammapadaViewModel = viewModel()) {
 
     val text by viewModel.currentText.collectAsState()
     val imageName by viewModel.currentImageName.collectAsState()
 
     val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val screenHeight = configuration.screenHeightDp.dp
     val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
     val constraints = if (isLandscape) {
-        landscapeConstraints()
+        landscapeConstraints(screenWidth, screenHeight)
     } else {
-        portraitConstraints()
+        portraitConstraints(screenWidth, screenHeight)
     }
 
     ConstraintLayout(
@@ -54,7 +58,6 @@ fun MainScreen(viewModel: DhammapadaViewModel = viewModel()){
         Box(
             modifier = Modifier
                 .layoutId("imageBox")
-                .size(400.dp)
                 .clip(RoundedCornerShape(16.dp)),
             contentAlignment = Alignment.Center
         ) {
@@ -64,52 +67,62 @@ fun MainScreen(viewModel: DhammapadaViewModel = viewModel()){
         Box(
             modifier = Modifier
                 .layoutId("textBox")
-                .width(400.dp)
                 .border(2.dp, Color(0xFF2C3930), RoundedCornerShape(16.dp))
                 .padding(8.dp),
             contentAlignment = Alignment.Center
         ) {
-            Text(text ?: PLACEHOLDER_TEXT,
-                style = TextStyle)
+            Text(
+                text ?: PLACEHOLDER_TEXT,
+                style = TextStyle
+            )
         }
     }
 }
 
-private fun portraitConstraints(): ConstraintSet {
+private fun portraitConstraints(screenWidth: Dp, screenHeight: Dp): ConstraintSet {
     return ConstraintSet {
         val imageBox = createRefFor("imageBox")
         val textBox = createRefFor("textBox")
+        val minScreenSize = min(screenWidth, screenHeight)
+        val boxSize = Dimension.value(minScreenSize * 0.85f)
 
         constrain(imageBox) {
-            top.linkTo(parent.top, margin = 50.dp)
+            top.linkTo(parent.top, margin = (screenHeight * 0.01f))
             start.linkTo(parent.start)
             end.linkTo(parent.end)
+            width = boxSize
+            height = boxSize
         }
 
         constrain(textBox) {
-            top.linkTo(imageBox.bottom, margin = 80.dp)
+            top.linkTo(imageBox.bottom, margin = (screenHeight * 0.01f))
             start.linkTo(parent.start)
             end.linkTo(parent.end)
+            width = boxSize
         }
     }
 }
 
-private fun landscapeConstraints(): ConstraintSet {
+private fun landscapeConstraints(screenWidth: Dp, screenHeight: Dp): ConstraintSet {
     return ConstraintSet {
         val imageBox = createRefFor("imageBox")
         val textBox = createRefFor("textBox")
+        val minScreenSize = min(screenWidth, screenHeight)
+        val boxSize = Dimension.value(minScreenSize * 0.6f)
 
         constrain(imageBox) {
-            top.linkTo(parent.top, margin = 50.dp)
+            top.linkTo(parent.top, margin = (screenHeight * 0.01f))
             start.linkTo(parent.start)
-            bottom.linkTo(parent.bottom, margin = 50.dp)
+            bottom.linkTo(parent.bottom)
+            width = boxSize
+            height = boxSize
         }
 
         constrain(textBox) {
-            top.linkTo(parent.top, margin = 50.dp)
-            start.linkTo(imageBox.end, margin = 50.dp)
-            end.linkTo(parent.end)
-            bottom.linkTo(parent.bottom, margin = 50.dp)
+            top.linkTo(parent.top, margin = (screenHeight * 0.05f))
+            start.linkTo(imageBox.end, margin = (screenWidth * 0.05f))
+            end.linkTo(parent.end, margin = (screenWidth * 0.05f))
+            bottom.linkTo(parent.bottom, margin = (screenHeight * 0.05f))
             width = Dimension.fillToConstraints
         }
     }
